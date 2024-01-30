30973051a8ca82ef9370d5bce3ce884e71648e19
Truecoderalert <141266977+Truecoderalert@users.noreply.github.com>
2024-01-30 15:36:30 -0500

    Add files via upload

---
+++ The Exam I passed/passed-exam.js
@@ -0,0 +1,20 @@
+function findit(){
+alert("You are looking for");
+}
+function heartclick(){
+var likes1 = document.querySelector(".likes1")
+likes1.innerText++;
+}
+
+function heartclicktwo(){
+var likes2 = document.querySelector(".likes2")
+likes2.innerText++;
+}
+function heartclickthree(){
+likes3=document.querySelector(".likes3")
+likes3.innerText++;
+}
+function hidethis(){
+hidethis = document.querySelector(".btntwo")
+hidethis.style.display = ("none");
+}
